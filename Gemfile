f4693f50644e77c8e956acc9dd8311d2830ff98f
Anton Jyha <antonjyha@gmail.com>
2015-06-23 15:21:07 +0300

    Increase performance

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'pry'
-gem 'rspec'
 gem 'method_profiler', '~> 2.0.1'
--- lib/dice_rate_calculator.rb
+++ lib/dice_rate_calculator.rb
@@ -1,37 +1,38 @@
 require 'pry'
 
 class DiceRateCalculator
-  attr_reader :dices_count, :sides_count, :sum_combinations
-
   def initialize(dices_count, sides_count)
     @dices_count = dices_count
     @sides_count = sides_count
   end
 
   def calculate(number)
-    initial_counts = Array.new(sides_count) { 1 }
-    count = combinations_count(initial_counts, 2, number)
-    puts count
-    count / (sides_count ** dices_count).to_f
+    if number.between?(@dices_count, @dices_count*@sides_count)
+      initial_counts = Array.new(@sides_count) { 1 }
+      count = combinations_count(initial_counts, 2, number)
+      count / (@sides_count ** @dices_count).to_f
+    else
+      raise 'Invalid number'
+    end
   end
 
   private
   def combinations_count(previous_counts, dice, number)
     current_counts = []
-    middle_value = (dice*sides_count - dice)/2 + dice
+    middle_value = (dice*@sides_count - dice)/2 + dice
 
-    (dice..middle_value ).each_with_index do |value, index|
-      start_index = (index+1 - sides_count < 0) ? 0 : index+1 - sides_count
+    (dice..middle_value).each_with_index do |value, index|
+      start_index = (index+1 - @sides_count < 0) ? 0 : index+1 - @sides_count
       current_counts << previous_counts[start_index..index].inject(:+)
-      return current_counts[index] if (value == number && dice == dices_count)
+      return current_counts[index] if (value == number && dice == @dices_count)
     end
 
-    if dice == dices_count
+    if dice == @dices_count
       index = dice.even? ? (middle_value-number-1) : (middle_value-number)
       current_counts[index]
     else
-      reversed_counts = dice.even? ? current_counts[0...-1].reverse : current_counts.reverse
-      combinations_count(current_counts + reversed_counts, dice+1, number)
+      remaining_counts = dice.even? ? current_counts[0...-1].reverse : current_counts.reverse
+      combinations_count(current_counts + remaining_counts, dice+1, number)
     end
   end
 end
