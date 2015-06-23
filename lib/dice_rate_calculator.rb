09b02758fad1edf2c44bba62546a375b0cdeafc0
Anton Jyha <antonjyha@gmail.com>
2015-06-23 15:15:51 +0300

    Implement using table method with half culculating

--- lib/dice_rate_calculator.rb
+++ lib/dice_rate_calculator.rb
@@ -1,4 +1,3 @@
-require_relative 'combination'
 require 'pry'
 
 class DiceRateCalculator
@@ -10,16 +9,29 @@ class DiceRateCalculator
   end
 
   def calculate(number)
-    if sum_combinations.keys.include?(number)
-      combinations_with_permutations_count = sum_combinations[number].inject(0) do |sum, combination|
-        sum + Combination.new(combination).permutations_count
-      end
-      combinations_with_permutations_count.to_f / (sides_count ** dices_count).to_f
-    end
+    initial_counts = Array.new(sides_count) { 1 }
+    count = combinations_count(initial_counts, 2, number)
+    puts count
+    count / (sides_count ** dices_count).to_f
   end
 
   private
-  def sum_combinations
-    @sum_combinations ||= (1..sides_count).to_a.repeated_combination(dices_count).group_by { |c| c.inject(:+)}
+  def combinations_count(previous_counts, dice, number)
+    current_counts = []
+    middle_value = (dice*sides_count - dice)/2 + dice
+
+    (dice..middle_value ).each_with_index do |value, index|
+      start_index = (index+1 - sides_count < 0) ? 0 : index+1 - sides_count
+      current_counts << previous_counts[start_index..index].inject(:+)
+      return current_counts[index] if (value == number && dice == dices_count)
+    end
+
+    if dice == dices_count
+      index = dice.even? ? (middle_value-number-1) : (middle_value-number)
+      current_counts[index]
+    else
+      reversed_counts = dice.even? ? current_counts[0...-1].reverse : current_counts.reverse
+      combinations_count(current_counts + reversed_counts, dice+1, number)
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require_relative 'lib/dice_rate_calculator'
 require 'method_profiler'
 
-dice_count = 15
+dice_count = 300
 sides_count = 6
-number = 100
+number = 900
 
 profiler = MethodProfiler.observe(DiceRateCalculator)
 result = DiceRateCalculator.new(dice_count, sides_count).calculate(number)
